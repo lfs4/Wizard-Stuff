55a5875eb70a23f39d8df368582ac04667c8f138
Lou <d4rksp4rtan1027@yahoo.com>
2014-10-20 00:06:59 -0400

    Finished Character controller

---
+++ PlayerScripts/Camera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public Transform player;
+	private float trackSpeed = 10;
+	
+	public void SetTarget(Transform t)
+	{
+		player = t;
+	}
+	
+	
+	void LateUpdate()
+	{
+		if(player)
+		{
+			float x = IncrementTowards(transform.position.x, player.transform.position.x, trackSpeed);
+			float y = IncrementTowards (transform.position.y , player.transform.position.y, trackSpeed);
+			transform.position = new Vector3 (x,y, transform.position.z); 
+		}	
+	}
+	private float IncrementTowards(float n, float target, float a)
+	{
+		if(n == target)
+		{
+			return n;
+		}
+		else
+		{
+			float dir = Mathf.Sign(target -n);
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target-n)) ? n: target;
+		}
+		
+	}
+}
--- PlayerScripts/Player.cs
+++ PlayerScripts/Player.cs
@@ -1,34 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : Entity {
 
-    public GameObject camera;
-    public Vector2 velocity;
-    float moveSpeed = 5;
+[RequireComponent (typeof(PlayerPhysics))]
+public class Player : MonoBehaviour {
+
+    public float gravity = 20;
+    public float speed = 8;
+    public float acceleration = 30;
+    public float jumpHeight = 12;
+    
+    private float currentSpeed;
+    private float targetSpeed;
+    private Vector2 amountToMove;
+    
+	private PlayerPhysics playerPhysics;
+
 
 
 	// Use this for initialization
-	public override void StartingActions () 
+	public void Start () 
 	{
-		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+		playerPhysics = GetComponent<PlayerPhysics>();
 	}
 	// Update is called once per frame
-	public override void UpdateActions () 
+	public void Update () 
 	{
 		CheckPlayerMovement();
 	}
-
-	void CheckPlayerMovement()
+	public void CheckPlayerMovement()
 	{
-		if (Input.GetKey (KeyCode.RightArrow))
+		if(playerPhysics.movementStopped)
 		{
-			transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));	
+			targetSpeed = 0;
+			currentSpeed = 0;
 		}
+			if(playerPhysics.grounded)
+			{
+				amountToMove.y = 0;
+				speed = 8;
+				if(Input.GetButtonDown("Jump"))
+				{
+					amountToMove.y = jumpHeight;
+				}
+			}
+			else 
+			{
+				speed = 4;
+			}
 			
-		else if (Input.GetKey (KeyCode.LeftArrow))
-		{
-			transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));	
+			targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+			currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+		
+			
+			amountToMove.x = currentSpeed;
+			amountToMove.y -= gravity * Time.deltaTime;
+			playerPhysics.Move(amountToMove * Time.deltaTime);
+			
 		}
+				
+	private float IncrementTowards(float n, float target, float a)
+	{
+		if(n == target)
+		{
+			return n;
+		}
+		else
+		{
+			float dir = Mathf.Sign(target -n);
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target-n)) ? n: target;
+		}
+	
 	}
 }
